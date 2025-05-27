f72166fd5881a9f44233eddebef31a141f1bd81c
Roland Awemo <awemo@publicplan.de>
2025-05-27 11:45:15 +0200

    feat: fix entrypoint

--- hello-world-docker-action/entrypoint.sh
+++ hello-world-docker-action/entrypoint.sh
@@ -2,8 +2,8 @@
 
 GREETING="Hello, $INPUT_WHO_TO_GREET!"
 
-echo "::notice file=entrypoint.sh,line=7::${GREETING}"
+echo "::notice file=entrypoint.sh,line=5::${GREETING}"
 
-echo "time=$(date)" >> $GITHUB_OUTPUT
+echo "time=$(date)" >>"$GITHUB_OUTPUT"
 
 exit 0
